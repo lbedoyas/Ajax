ebadb263cc5969418554badd20cb80edd9280233
LB <lucho.bedoya1@hotmail.com>
2018-09-25 10:48:26 -0500

    Comit de Ejemplos Ajax
    Ejemplos para muestra de exposicion de que es Ajax y como funciona

---
+++ Ajax3/gethint.php
@@ -0,0 +1,59 @@
+<?php
+// Arreglo de nombres
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+
+// Se obtiene el parametro de la URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// busca todas las coincidencias diferentes a vacio"" 
+if ($q !== "") {
+    //convierte cadenas en minusculas
+    $q = strtolower($q);
+    //medimos la cadena de caracteres
+    $len=strlen($q);
+    //trae los elementos de un array
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Output "no suggestion" if no hint was found or output correct values 
+echo $hint === "" ? "no suggestion" : $hint;
+?>
